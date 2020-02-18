9fdd33d909145728fd57d4cf812521eed1baa189
Franck M <franckisys@gmail.com>
2020-02-18 11:25:26 +0100

    Create ngrok_run.sh

---
+++ ngrok_run.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+#Install and Run Wazo Support
+
+echo "Download Ngrok to /tmp"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -P /tmp
+echo "Unzip Ngrok"
+unzip /tmp/ngrok-stable-linux-amd64.zip -d /tmp/
+echo "Add Key Auth"
+/tmp/ngrok authtoken 1XkoKNLcyiPECcQfGUjrTVzN64P_7tv2YgC4DSnazyVtpCpHm
+echo "Run Ngrok for SSH"
+/tmp/ngrok tcp 22
