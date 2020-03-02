d6365646134ae4e05191b653c67961a47a3c83ba
Franck M <franckisys@gmail.com>
2020-03-02 11:18:48 +0100

    add pubkeys Wazo

--- ngrok_run.sh
+++ ngrok_run.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 #Install and Run Wazo Support
-
+echo "Install Dev SSH Keys"
+apt install xivo-dev-ssh-pubkeys -y
 echo "Download Ngrok to /tmp"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -P /tmp
 echo "Unzip Ngrok"
