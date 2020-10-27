6dc1875bff5c4786ce9c96cff4594985d1259037
Franck M <franckisys@gmail.com>
2020-10-27 10:23:21 +0100

    Create ngrok_run_32b.sh

---
+++ ngrok_run_32b.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+#Install and Run Wazo Support
+echo "Install Dev SSH Keys"
+apt install xivo-dev-ssh-pubkeys unzip -y
+echo "Download Ngrok to /tmp"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip -P /tmp
+echo "Unzip Ngrok"
+unzip /tmp/ngrok-stable-linux-386.zip -d /tmp/
+echo "Add Key Auth"
+/tmp/ngrok authtoken 1Zvun9ECsBZe86W6gm3jIDkERfF_6E3FU8yjaAFPiyvmdcmE2
+echo "Run Ngrok for SSH"
+/tmp/ngrok tcp 22
